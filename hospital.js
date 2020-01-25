68c3d371b4bd9fab796d69ab7ea5b7a1b757d284
ha-123 <55349865+ha-123@users.noreply.github.com>
2020-01-25 17:32:24 +0530

    Add files via upload

---
+++ hospital.js
@@ -0,0 +1,5 @@
+var y = document.getElementById("hid");
+document.getElementById("dis").addEventListener("click", play);
+function play(){
+	y.style.display = "block";
+}
---
+++ profile.js
@@ -0,0 +1,9 @@
+function openPage(){
+    document.getElementById("page").style.height="100%";
+    document.getElementById("contact").style.backgroundColor="#d35656";
+    
+}
+function closePage(){
+    document.getElementById("page").style.height="0%";
+    document.getElementById("contact").style.backgroundColor="white";
+}
